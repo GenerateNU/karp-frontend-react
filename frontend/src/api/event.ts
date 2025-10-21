d3d64d7ed766d9291d153e1c1048c7a6e95779f6
Sierra Welsch <sierrasathi4@gmail.com>
2025-10-21 16:40:22 -0400

    did this not push idk

--- frontend/src/api/event.ts
+++ frontend/src/api/event.ts
@@ -46,8 +46,6 @@ export async function uploadEventImage(
     'GET',
   );
 
-  console.log('Presigned data:', presignedData);
-
   const uploadResponse = await fetch((await presignedData).upload_url, {
     method: 'PUT',
     headers: {
