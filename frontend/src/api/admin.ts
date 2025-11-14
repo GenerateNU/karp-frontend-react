cea72070fbcb15ba7190a303a1941e8e479be58a
qihongw08 <qihongw08@gmail.com>
2025-11-14 18:55:30 -0500

    admin comment

--- frontend/src/api/admin.ts
+++ frontend/src/api/admin.ts
@@ -1,6 +1,7 @@
 import { makeRequest } from '@/api/base';
 import type { AdminProfile } from '@/types/user';
 
+// get all admins
 export async function getAllAdmins(): Promise<AdminProfile[]> {
   return makeRequest<AdminProfile[]>('/admin/all', 'GET');
 }
