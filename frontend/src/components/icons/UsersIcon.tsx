5b941729c04962b9cc2f57a876ecc7f226ebe868
patela22 <39590072+patela22@users.noreply.github.com>
2025-11-15 12:08:28 -0500

    SVG Hell

---
+++ frontend/src/components/icons/UsersIcon.tsx
@@ -0,0 +1,46 @@
+export const UsersIcon = ({ className = '' }: { className?: string }) => (
+  <svg
+    width="50"
+    height="50"
+    viewBox="0 0 50 50"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <g clipPath="url(#clip0_3910_3463)">
+      <path
+        d="M35.4166 43.75V39.5833C35.4166 37.3732 34.5386 35.2536 32.9758 33.6908C31.413 32.128 29.2934 31.25 27.0833 31.25H10.4166C8.20645 31.25 6.08683 32.128 4.52403 33.6908C2.96123 35.2536 2.08325 37.3732 2.08325 39.5833V43.75"
+        stroke="#1D0F48"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.7501 22.9167C23.3525 22.9167 27.0834 19.1857 27.0834 14.5833C27.0834 9.98096 23.3525 6.25 18.7501 6.25C14.1477 6.25 10.4167 9.98096 10.4167 14.5833C10.4167 19.1857 14.1477 22.9167 18.7501 22.9167Z"
+        stroke="#1D0F48"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M47.9167 43.75V39.5833C47.9154 37.7369 47.3008 35.9433 46.1696 34.484C45.0384 33.0247 43.4545 31.9824 41.6667 31.5208"
+        stroke="#1D0F48"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M33.3333 6.52084C35.1258 6.9798 36.7146 8.0223 37.8492 9.48398C38.9838 10.9457 39.5996 12.7434 39.5996 14.5938C39.5996 16.4441 38.9838 18.2418 37.8492 19.7035C36.7146 21.1652 35.1258 22.2077 33.3333 22.6667"
+        stroke="#1D0F48"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_3910_3463">
+        <rect width="50" height="50" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
